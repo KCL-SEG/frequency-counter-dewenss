a3c4b47a92d743df5d171ed08f0fbbd6f62ad2c1
Dylan <k22026193@kcl.ac.uk>
2023-09-29 13:05:56 +0100

    add code to complete it's function

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,14 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    count = 0
+    for i in items:
+        item_str = str(i)
+        if item_str in frequencies:
+            frequencies[item_str]+= 1
+        else:
+            frequencies[item_str] = 1
+    print(frequencies)
     return frequencies
+
+frequencies(["a","b","a","a"])
